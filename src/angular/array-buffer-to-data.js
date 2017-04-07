82068b3548c0de695fb7c6d334d6205bc68dc511
EELokets <EELokets@1cbit.ru>
2017-04-07 16:54:24 +0300

    Added check for toString.

--- src/angular/array-buffer-to-data.js
+++ src/angular/array-buffer-to-data.js
@@ -1,35 +1,40 @@
 (function(window, angular) {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('array-buffer-to-data', [])
-        .factory('arrayBufferToData', function () {
-            return {
-                toBase64: function (arrayBuffer) {
-                    var binary = '';
-                    var bytes = new Uint8Array(arrayBuffer);
-                    var len = bytes.byteLength;
-                    for (var i = 0; i < len; i++) {
-                        binary += String.fromCharCode(bytes[i]);
-                    }
-                    return window.btoa(binary);
-                },
+  angular
+    .module('array-buffer-to-data', [])
+    .factory('arrayBufferToData', function () {
+      return {
+        toBase64: function (arrayBuffer) {
+          var binary = '';
+          var bytes = new Uint8Array(arrayBuffer);
+          var len = bytes.byteLength;
+          for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+          }
+          return window.btoa(binary);
+        },
 
-                toSting: function (arrayBuffer) {
-                    var base64 = this.toBase64(arrayBuffer);
+        toString: function (arrayBuffer) {
+          try {
+            var base64 = this.toBase64(arrayBuffer);
 
-                    return decodeURIComponent(escape(window.atob(base64)));
-                },
+            return decodeURIComponent(escape(window.atob(base64)));
+          } catch (e) {
+            console.warn('Can not be converted to String');
+            return false;
+          }
+        },
 
-                toJSON: function (arrayBuffer) {
-                    try {
-                        var string = this.toSting(arrayBuffer);
-                        return JSON.parse(string);
-                    }
-                    catch (e) {
-                        return {};
-                    }
-                }
-            }
+        toJSON: function (arrayBuffer) {
+          try {
+            var string = this.toString(arrayBuffer);
+            return JSON.parse(string);
+          } catch (e) {
+            console.warn('Can not be converted to JSON');
+            return false;
+          }
+        }
+      }
     });
 })(window, window.angular);
--- src/native/array-buffer-to-data.js
+++ src/native/array-buffer-to-data.js
@@ -1,39 +1,44 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    var root = this;
+  var root = this;
 
-    var arrayBufferToData = {
-        toBase64: function (arrayBuffer) {
-            var binary = '';
-            var bytes = new Uint8Array(arrayBuffer);
-            var len = bytes.byteLength;
-            for (var i = 0; i < len; i++) {
-                binary += String.fromCharCode(bytes[i]);
-            }
-            return window.btoa(binary);
-        },
+  var arrayBufferToData = {
+    toBase64: function (arrayBuffer) {
+      var binary = '';
+      var bytes = new Uint8Array(arrayBuffer);
+      var len = bytes.byteLength;
+      for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+      }
+      return window.btoa(binary);
+    },
 
-        toString: function (arrayBuffer) {
-            var base64 = this.toBase64(arrayBuffer);
+    toString: function (arrayBuffer) {
+      try {
+        var base64 = this.toBase64(arrayBuffer);
 
-            return decodeURIComponent(escape(window.atob(base64)));
-        },
+        return decodeURIComponent(escape(window.atob(base64)));
+      } catch (e) {
+        console.warn('Can not be converted to String');
+        return false;
+      }
+    },
 
-        toJSON: function (arrayBuffer) {
-            try {
-                var string = this.toSting(arrayBuffer);
-                return JSON.parse(string);
-            }
-            catch (e) {
-                return {};
-            }
-        }
-    };
-
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = arrayBufferToData;
-    } else {
-        root.arrayBufferToData = arrayBufferToData;
+    toJSON: function (arrayBuffer) {
+      try {
+        var string = this.toString(arrayBuffer);
+        return JSON.parse(string);
+      } catch (e) {
+        console.warn('Can not be converted to JSON');
+        return false;
+      }
     }
+  };
+
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = arrayBufferToData;
+  } else {
+    root.arrayBufferToData = arrayBufferToData;
+  }
 }).call(this);
